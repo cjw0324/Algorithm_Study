d400fff4270b60e5f62d119bb14a89776f2c2936
choijaewoo <cchoi00332244@gamil.com>
2024-10-05 11:25:27 +0900

    "24.10.05 clear 2839"

--- src/baekjoon/브루트포스/Baekjoon2839.java
+++ src/baekjoon/브루트포스/Baekjoon2839.java
@@ -1,7 +1,30 @@
 package baekjoon.브루트포스;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Baekjoon2839 {
-    public static void main(String[] args) {
-        System.out.println(2839);
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        int max = 1667;
+        int x = 0, y = 0;
+        for (int i = 0; i < 1667; i++) {
+            for (int j = 0; j < 1001; j++) {
+                int sum = i * 3 + j * 5;
+                if (sum == N && max > i + j) {
+                    max = i + j;
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        if (max == 1667) {
+            System.out.println("-1");
+        } else {
+            System.out.println(x + y);
+        }
     }
 }
